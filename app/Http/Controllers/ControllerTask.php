ec94f3677c33da78a0d9a6885996895921bff0e9
Dumilson <domingosbreganha9@gmail.com>
2022-02-19 04:52:55 +0100

    Registrando as interfaces nos Provideres

--- app/Http/Controllers/ControllerTask.php
+++ app/Http/Controllers/ControllerTask.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\TaskService;
 use Illuminate\Http\Request;
 
 class ControllerTask extends Controller
 {
-    //
+    private $serviceTask;
+
+    public function __construct(TaskService $serviceTask){
+        $this->serviceTask = $serviceTask;
+    }
+    public function getList(){
+        return $this->getList();
+    }
+    public function store(Request $request){
+        $data = $request->all();
+        $this->serviceTask->store($data);
+    }
+    public function get($id){
+        $this->serviceTask->get($id);
+    }
+    public function destroy($id){
+        $this->serviceTask->destroy($id);
+    }
+    public function update(Request $request, $id){
+        $data = $request->all();
+        $this->serviceTask->update($data,$id);
+    }
 }
